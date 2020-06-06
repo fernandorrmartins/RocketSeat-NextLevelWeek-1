45011b8fd7b09de801190c1a1ba3aebd4e6478a1
Fernando R. R <32397071+fernandorrmartins@users.noreply.github.com>
2020-06-05 23:43:38 -0300

    -Finalizado a criação das rotas da aplicação; -Configurado cors;

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 
-const routes = express.Router();
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
-});
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+// index (listagem), show (um item apenas), create/store, update, delete/destroy
 
 export default routes;
+
+// Service Pattern
+// Repository Patter (Data Mapper)
