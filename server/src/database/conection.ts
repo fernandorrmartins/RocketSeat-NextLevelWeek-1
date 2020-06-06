fb59d32bbce8322cb6f3674d4c5fb21b09a4f4a8
Fernando R. R <32397071+fernandorrmartins@users.noreply.github.com>
2020-06-05 21:54:44 -0300

    -Criação das tabelas do projeto finalizado;

--- server/src/database/conection.ts
+++ server/src/database/conection.ts
@@ -4,13 +4,14 @@ import path from 'path';
 const connection = knex({
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname,  'database.sqlite'),
+        filename: path.resolve(__dirname, 'database.sqlite'),
     },
+    useNullAsDefault: true,
 });
 
 export default connection;
 
-// Migrations = Histórico dobanco de dados
+// Migrations = Histórico do banco de dados
 
 // create table points
 // create table users
